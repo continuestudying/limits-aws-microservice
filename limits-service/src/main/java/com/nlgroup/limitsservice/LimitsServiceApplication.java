d8555a6b3a8d29d0a63259f13d3b6563ee17d9e0
continuestudying <continuestudying@gmail.com>
2019-09-24 10:19:24 +0700

    Deploy all service on AWS for testing

--- limits-service/src/main/java/com/nlgroup/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/nlgroup/limitsservice/LimitsServiceApplication.java
@@ -2,10 +2,12 @@ package com.nlgroup.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-//@EnableHystrix
+@EnableDiscoveryClient
+@EnableHystrix
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
